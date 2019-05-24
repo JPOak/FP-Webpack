4f1b04dffd3ca0ad1a50bfdc0dcc7c1ec4045b81
JP Mac <josh@whosjosh.com>
2019-05-24 12:01:28 -0700

    Initial Init.

---
+++ src/build/common.config.js
@@ -0,0 +1,38 @@
+const webpack = require('webpack');
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+
+	entry: {app:[path.resolve(__dirname, '../assets/js/app.js'), path.resolve(__dirname, '../assets/scss/app.scss')]},
+	performance: {
+		hints: false
+  },
+  stats: {
+    hash: false,
+    version: false,
+    timings: false,
+    children: false,
+    chunks: false,
+    modules: false,
+    source: false,
+    publicPath: false
+  },
+	module: {
+		rules: [
+			{
+					test: /\.js$/,
+					use: 'babel-loader',
+					exclude: /node_modules/
+			}
+		]
+	},
+	plugins: [
+		new FriendlyErrorsWebpackPlugin(),
+		new webpack.ProvidePlugin({
+			$: 'jquery',
+			jQuery: 'jquery'
+		})
+	]
+
+};//Config end
